0ffe5104d3bec62135668620b75e46c5dc8a8116
501 <501@academicos.cesde.edu.co>
2020-02-14 21:01:21 -0500

    Esta es la versión funcional de mi codigo

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//Esto es un comentario en Linea
+
+/* Esto es un comentario de varias Lineas
+En este caso son 3 Lineas */
+
+// La variable
+/* Las variables nos permiten alacenar valores en memoria que pueden cambiar 
+durante la ejecución de nuestro codigo */
+
+
+$nombre="Julian Andres ";
+//echo($nombre);
+$edad=28;
+//echo($edad);
+$estatura=1.65;
+//echo($estatura);
+
+
+echo("Hola Mundo ");
+
+echo("Bienvenido, su nombre es: ".$nombre." tiene ".$edad. " y su estatura es: ".$estatura);
+print("<br>");
+print ("Bienvenido, su nombre es: ".$nombre." tiene ".$edad. " y su estatura es: ".$estatura);
+
+
+?>
